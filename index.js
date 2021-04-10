949354ed939cdd50d941473782aebd2167e75920
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-10 16:20:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(driverArray, name){
+    const matching = driverArray.filter(driver => {
+        if (driver === name){ 
+            return driver
+        } else if(driver.toLowerCase() === name.toLowerCase()){
+            return driver
+        }
+    })
+    return matching
+}
+
+function fuzzyMatch(driverArray, name){
+    const matching = driverArray.filter(driver => {
+        return driver.charAt(0).toLowerCase() + driver.charAt(1).toLowerCase() === name.charAt(0).toLowerCase() + name.charAt(1).toLowerCase()
+    })
+    return matching
+}
+
+function matchName(driverArray, name){
+    const matching = driverArray.filter(driver => {
+        return driver.name.toLowerCase() === name.toLowerCase()
+    })
+    return matching
+}
